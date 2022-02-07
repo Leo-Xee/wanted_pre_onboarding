2c277230e6d069614488434539cb28ce7f461af3
Jangmin Lee <jang.xmin@gmail.com>
2022-02-08 05:29:52 +0900

    :sparkles: feat: toggle button 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Toggle from "./components/Toggle";
+
 function App() {
-  return <div className="text-3xl font-bold underline">hello world</div>;
+  return (
+    <div className="flex flex-col justify-center">
+      <Toggle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Toggle() {
+  const [isToggled, setIsToggled] = useState(false);
+  const text = isToggled ? "ON" : "OFF";
+
+  const onToggle = () => {
+    setIsToggled((prev) => !prev);
+  };
+
+  return (
+    <div className="flex flex-col items-center mt-40">
+      <label className="cursor-pointer">
+        <div className="relative">
+          <input type="checkbox" onClick={onToggle} className="sr-only" />
+          <div
+            className={`w-20 h-10 bg-gray-300 rounded-full ${
+              isToggled ? "bg-violet-700" : ""
+            } ease-linear duration-100`}
+          ></div>
+          <div
+            className={`absolute animate-toggle left-1 top-1 w-8 h-8 bg-white rounded-full ${
+              isToggled ? "translate-x-10" : "translate-x-0"
+            } ease-linear duration-100`}
+          ></div>
+        </div>
+      </label>
+      <span className="mt-2">Toggle Switch {text}</span>
+    </div>
+  );
+}
+
+export default Toggle;
